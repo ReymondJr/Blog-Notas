ba8f8e13eb435124894a4602ee0ecdb3df176e94
Retro150 <reymondjradames.com>
2023-10-09 18:39:46 -0400

    mejorada

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Tarea from './componentes/Tareas';
+import { Final } from './componentes/footer';
 
 function App() {
   return (
     <>
      <Tarea />
+     <Final />
     </>
   );
 }
---
+++ src/componentes/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Final = () => {
+  return (
+    <div>
+        <h2>
+            Desarrollado por Retro15
+        </h2>
+    </div>
+  )
+}
